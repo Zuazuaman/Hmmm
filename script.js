e80aa0bbe8312cb2ad1765f7873b799ecab2c13d
Zuazuaman <140221328+Zuazuaman@users.noreply.github.com>
2023-08-20 14:35:01 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const messageBox = document.getElementById("message-box");
+const inputMessage = document.getElementById("input-message");
+const sendButton = document.getElementById("send-button");
+
+const socket = new WebSocket("wss://your-github-repo-name.herokuapp.com");
+
+socket.addEventListener("open", () => {
+    console.log("Connected to server");
+});
+
+socket.addEventListener("message", (event) => {
+    const message = event.data;
+    messageBox.innerHTML += `<p>${message}</p>`;
+    messageBox.scrollTop = messageBox.scrollHeight;
+});
+
+sendButton.addEventListener("click", () => {
+    const message = inputMessage.value;
+    socket.send(message);
+    inputMessage.value = "";
+});
